bc03ddb01c309a585c0c4f7673d10748acae2132
xerohead <shane.jordan.407@my.csun.edu>
2021-02-03 10:37:07 -0800

    Week 2 Lab

---
+++ Week_2/Script/Week 2 Lab.R
@@ -0,0 +1,15 @@
+#02/03/2021
+#Week 2 lab
+#Created by Shane E. Jordan
+
+#load in libraries
+library(tidyverse)
+library(here)
+
+#read in data
+weight<-read_csv(here("Week_2","Data","weightdata.csv")) #tidyverse code
+weight<-read.csv("Week_2/Data/weightdata.csv") #base R code
+view(weight)
+#data analysis
+head(weight)
+tail(weight)
